f839a53b4e13947038227b8b88156d3cd40fdae1
Byron Slabach <byron.slabach@gmail.com>
2019-03-18 14:48:36 -0400

    add rivals data

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 # import modules
 import pandas as pd
 from lxml import html
-from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.options import Options
-from bs4 import BeautifulSoup
-import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from fuzzywuzzy import fuzz
 
 
 class App:
-    def __init__(self, teams_path, output_path, cur_year, team):
-        self.teamsPath = teams_path
+    def __init__(self, output_path, cur_year, team):
         self.output_path = output_path
         self.cur_year = cur_year
         self.team = team
 
     def get_recruit_data(self):
-        # gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
-        # binary = FirefoxBinary(r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
+
         cap = DesiredCapabilities().FIREFOX
         cap["marionette"] = True
-        driver = webdriver.Firefox(capabilities=cap, executable_path="C:\\Users\\bslabach\\PycharmProjects\\RecruitingData\\geckodriver.exe")
         options = Options()
         options.add_argument('--headless')
+        driver = webdriver.Firefox(capabilities=cap,
+                                   executable_path="C:\\Users\\bslabach\\PycharmProjects\\RecruitingData\\geckodriver.exe",
+                                   options=options)
 
         driver.get("https://247sports.com/college/"+self.team+"/Season/"+str(self.cur_year)+"-Football/Offers/")
         show_more_xpath = \
@@ -41,14 +42,15 @@ class App:
         offers_list = html_tree.xpath(recruit_xpath)
 
         list_of_results = [('Recruit Name',
-                            '247 Link',
                             'Position',
                             'High School',
                             'Metrics',
                             'Composite Rating',
                             '247 Rating',
+                            'Rivals Rating',
                             'Status',
-                            'Crystal Ball / %')]
+                            'Crystal Ball: Pick / %',
+                            '247 Link')]
 
         for element in offers_list:
 
@@ -92,9 +94,21 @@ class App:
                 tfs_rating = "N/A"
 
             try:
-                tfs_cb_pick = tfs_tree.xpath('//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[2]/span[1]/text()')
-                tfs_cb_perc = tfs_tree.xpath('//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[2]/span[2]/text()')
-                # print(tfs_cb_perc)
+                tfs_cb_list_type = tfs_tree.xpath(
+                    '//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/@class')
+
+                # print(tfs_cb_list_type[0])
+
+                if tfs_cb_list_type[0] == 'prediction-list long':
+                    tfs_cb_pick = tfs_tree.xpath(
+                        '//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[1]/span[1]/text()')
+                    tfs_cb_perc = tfs_tree.xpath(
+                        '//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[1]/span[2]/text()')
+                else:
+                    tfs_cb_pick = tfs_tree.xpath(
+                        '//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[2]/span[1]/text()')
+                    tfs_cb_perc = tfs_tree.xpath(
+                        '//*[@id="page-content"]/div[1]/section/header/section[2]/section[2]/ul[1]/li[2]/span[2]/text()')
 
                 if not tfs_rating:
                     cb_info = "none"
@@ -110,15 +124,51 @@ class App:
 
             driver2.quit()
 
+            rivals_search_url = 'https://n.rivals.com/search'
+            driver3 = webdriver.Firefox(capabilities=cap,
+                                        executable_path="C:\\Users\\bslabach\\PycharmProjects\\RecruitingData\\geckodriver.exe",
+                                        options=options)
+            driver3.get(rivals_search_url)
+
+            editor = WebDriverWait(driver3, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="search_query"]')))
+            editor.send_keys(recruit_name.strip())
+            editor.submit()
+
+            WebDriverWait(driver3, 10).until(
+                EC.visibility_of_element_located((By.XPATH, '//*[@id="content_"]')))
+
+            rvls_fname = driver3.find_element_by_css_selector('div.first-name.ng-binding').text
+            rvls_lname = driver3.find_element_by_css_selector('div.last-name.ng-binding').text
+            rvls_name = rvls_fname + " " + rvls_lname
+            # rvls_year = driver3.find_element_by_css_selector('td.year > span.pos.ng-binding.ng-scope').text
+
+            diff_ratio = fuzz.token_sort_ratio(rvls_name,recruit_name.strip())
+
+            # print(f"{rvls_fname} {rvls_lname}, {rvls_year}, {rvls_rating}")
+            print(f"{diff_ratio}")
+
+            if diff_ratio > 90:
+                rvls_rating = driver3.find_element_by_css_selector('td.rating > span.pos.ng-binding.ng-scope').text
+
+                if not rvls_rating:
+                    rvls_rating = "N/A"
+            else:
+                rvls_rating = "Look up manually"
+
+            driver3.quit()
+
             recruit_data = (recruit_name.strip(),
-                            recruit_url_trimmed,
                             recruit_pos.strip(),
                             recruit_hs.strip(),
                             recruit_metrics.strip(),
                             recruit_rating.strip(),
                             tfs_rating,
+                            rvls_rating,
                             recruit_status,
-                            cb_info)
+                            cb_info,
+                            recruit_url_trimmed)
+
             # except:
             #     recruit_data = ("This",
             #                     "is",
@@ -147,7 +197,7 @@ def main():
     team = "purdue"
     output_file = "lib/output-"+team+"-"+str(year)+".csv"
 
-    a = App("lib/teams-fbs.json", output_file, year, team)
+    a = App(output_file, year, team)
 
     a.get_recruit_data()
 
